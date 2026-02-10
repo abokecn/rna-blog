b14da101aff9a5b1f5101e5fdadfb2bb14f918ab
慈父斯大林 <abokecn@hotmail.com>
2026-02-11 00:03:41 +0800

    Update site configuration for RNA Blog

--- site.config.mjs
+++ site.config.mjs
@@ -11,11 +11,11 @@ if (!hasSiteUrl && process.env.NODE_ENV === 'production') {
 
 export const site = {
   url: hasSiteUrl ? siteUrl : fallbackSiteUrl,
-  title: 'Astro Themes by Whono',
-  brandTitle: 'Whono',
-  author: 'Whono',
+  title: ' RNA Blog ',
+  brandTitle: 'RNA',
+  author: 'RNA',
   authorAvatar: 'author/avatar.webp',
-  description: '一个 Astro 主题的展示站：轻量、可维护、可复用。'
+  description: 'The RNA Blog'
 };
 
 export const PAGE_SIZE_ARCHIVE = 12;
